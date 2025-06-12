913ffc65987bddaaaf2efa5f1a33f3bf6b6f8a8d
Yousef Ahmed <floky4444@gmail.com>
2025-06-12 21:57:51 +0300

    main

--- src/components/fragments/home/NewFeatures.jsx
+++ src/components/fragments/home/NewFeatures.jsx
@@ -13,32 +13,6 @@ function NewFeatures() {
           </h2>
           <p>Our app offers powerful AI tools designed to simplify and elevate your image editing experience. Whether you're creating, resizing, or cleaning up images, these features help you save time and achieve professional results with ease.</p>
         </div>
-        <div className="plans">
-          <div className="plan">
-            <h2>Free</h2>
-            <span>For any kind of project or design work.</span>
-            <h3>00.00</h3>
-            <Link to={"/"} className="btn w100">
-              Register now
-            </Link>
-          </div>
-          <div className="plan">
-            <h2>Monthly</h2>
-            <span>auto-renewing subscription</span>
-            <h3>20.99</h3>
-            <Link to={"/"} className="btn w100">
-              Register now
-            </Link>
-          </div>
-          <div className="plan">
-            <h2>Free</h2>
-            <span>auto-renewing subscription</span>
-            <h3>20.99</h3>
-            <Link to={"/"} className="btn w100">
-              Register now
-            </Link>
-          </div>
-        </div>
       </div>
     </div>
   );
