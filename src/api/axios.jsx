397d79dcd441a907f00f5b942a1f2dd38f51bff2
Yousef Ahmed <floky4444@gmail.com>
2025-06-18 18:35:00 +0300

    Update axios.jsx

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 const isDev = import.meta.env.DEV;
 
 export const AxiosInstance = axios.create({
-  baseURL: isDev ? "/api" : "https://admin.sigmaagencyjo.com/api/v1",
+  baseURL: isDev ? "/api" : "https://cors-anywhere.herokuapp.com/https://admin.sigmaagencyjo.com/api/v1",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
@@ -11,7 +11,7 @@ export const AxiosInstance = axios.create({
 });
 
 export const AxiosBG = axios.create({
-  baseURL: isDev ? "/bg" : "https://3rabapp.com/apps",
+  baseURL: isDev ? "/bg" : "https://cors-anywhere.herokuapp.com/https://3rabapp.com/apps",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
