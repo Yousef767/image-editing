c32d49217284add1eff29cedccf422f6c8d0bf41
Yousef Ahmed <floky4444@gmail.com>
2025-06-19 19:32:27 +0300

    main

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,20 +1,17 @@
 import axios from "axios";
-const isDev = import.meta.env.DEV;
 
 export const AxiosInstance = axios.create({
-  baseURL: isDev ? "/api" : "https://cors-anywhere.herokuapp.com/https://admin.sigmaagencyjo.com/api/v1",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    "Access-Control-Allow-Origin": "*",
   },
 });
 
 export const AxiosBG = axios.create({
-  baseURL: isDev ? "/bg" : "https://cors-anywhere.herokuapp.com/https://3rabapp.com/apps",
+  baseURL: "/bg",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    "Access-Control-Allow-Origin": "*",
   },
 });
